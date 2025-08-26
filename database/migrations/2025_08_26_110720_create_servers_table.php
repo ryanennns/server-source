851c31eb2b1d01275bee906798b80c2be4ba86c3
Ryan Enns <ryanennns@gmail.com>
2025-08-26 07:11:28 -0400

    filament init

---
+++ database/migrations/2025_08_26_110720_create_servers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('ip')->nullable();
+            $table->integer('port')->nullable();
+            $table->string('ec2_instance_id')->nullable();
+            $table->string('region')->nullable();
+            $table->string('instance_type')->nullable();
+            $table->json('tags')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
